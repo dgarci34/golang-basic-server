59325c63f6bf9b2b6f8773cf8861736154ece6f8
dgarci34 <dgarcia.g385@gmail.com>
2023-05-07 17:44:04 -0700

    added functionality to get basic stock info for aapl and outputting it

--- server.go
+++ server.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
 func getApiKey() string {
 	// TODO this key is only a demo, register and get a real api key
-	return "https://eodhistoricaldata.com/api/fundamentals/AAPL.US?api_token=demo"
+	return "https://eodhistoricaldata.com/api/eod/MCD.US?from=2023-05-05&to=2023-05-05&period=d&fmt=json&api_token=demo"
+}
+
+type StockDayInfo struct {
+	Date           string
+	Open           float64
+	High           float64
+	Low            float64
+	Close          float64
+	Adjusted_close float64
+	Volume         int
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -31,10 +43,39 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "POST request successful\n")
 	stock := r.FormValue("Stock")
 
+	if len(stock) == 0 {
+		log.Fatalln("Error: stock name not specified")
+	}
+
 	fmt.Fprintf(w, "Stock name = %s\n", stock)
+
+	resp, err := http.Get(getApiKey())
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var dayInfoArr []StockDayInfo
+	json.Unmarshal(body, &dayInfoArr)
+	dayInfo := dayInfoArr[0]
+
+	fmt.Fprintf(w, "Date: %s\nOpen: %.2f\nHigh: %.2f\nLow: %.2f\nClose: %.2f\nAdjusted Close: %.2f\nVolume: %d\n",
+		dayInfo.Date,
+		dayInfo.Open,
+		dayInfo.High,
+		dayInfo.Low,
+		dayInfo.Close,
+		dayInfo.Adjusted_close,
+		dayInfo.Volume)
+
+	fmt.Println("Recieved form handle request")
 }
 
 func main() {
